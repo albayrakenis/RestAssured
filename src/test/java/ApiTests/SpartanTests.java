62c8fae4cd287a65d0cd01b5b50964d9cd33ce94
Enis ALBAYRAK <enis.albayrak@ogr.iu.edu.tr>
2022-04-05 11:18:17 +0300

    RestAssure

---
+++ src/test/java/ApiTests/SpartanTests.java
@@ -0,0 +1,26 @@
+package ApiTests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class SpartanTests {
+
+    String spartanBaseUrl = "http://54.167.157.13:8000";
+    @Test
+    public void viewSpartanTest1 (){
+
+        Response response = RestAssured.get(spartanBaseUrl + "/api/spartans" );
+
+
+        System.out.println(response.statusCode());
+
+
+        System.out.println(response.body().prettyPrint());
+
+        //System.out.println(response.body().asString());
+
+
+    }
+
+}
